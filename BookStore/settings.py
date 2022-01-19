0d9bb9aeadf09a4e67023e816abae05a3d7f9f91
xiaowang012 <1300202481@qq.com>
2022-01-19 17:30:06 +0800

    将数据库配置的127.0.0.1改为localhost

--- BookStore/settings.py
+++ BookStore/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     { 
         'ENGINE': 'django.db.backends.mysql',    # 数据库引擎
         'NAME': 'django_database', # 数据库名称
-        'HOST': '127.0.0.1', # 数据库地址，本机 ip 地址 127.0.0.1 
+        'HOST': 'localhost', # 数据库地址，本机 ip 地址 127.0.0.1 
         'PORT': 3306,
         'USER': 'root',  # 数据库用户名
         'PASSWORD': '123456', # 数据库密码
